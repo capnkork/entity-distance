a400ed16205b68b98060cec6f386a25107be8996
capnkork <me@capnkork.com>
2021-07-20 19:47:23 -0400

    Backport to 1.16, closes #2

--- src/main/java/com/capnkork/entitydistance/config/ModMenuIntegration.java
+++ src/main/java/com/capnkork/entitydistance/config/ModMenuIntegration.java
@@ -1,7 +1,7 @@
 package com.capnkork.entitydistance.config;
 
-import com.terraformersmc.modmenu.api.ConfigScreenFactory;
-import com.terraformersmc.modmenu.api.ModMenuApi;
+import io.github.prospector.modmenu.api.ConfigScreenFactory;
+import io.github.prospector.modmenu.api.ModMenuApi;
 
 public class ModMenuIntegration implements ModMenuApi {
     @Override
